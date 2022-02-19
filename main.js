d52715f7484fc4d85adaabfa5e6efe13e849ecf6
BunnyMeee <75510733+BunnyMeee@users.noreply.github.com>
2022-02-18 20:41:14 -0700

    Add files via upload

--- main.js
+++ main.js
@@ -1,3 +1,6 @@
+nose_x = 0;
+nose_y = 0;
+
 function setup() {
     canvas = createCanvas(300, 300);
     canvas.center();
@@ -13,9 +16,11 @@ function modelLoaded() {
 }
 function gotPoses(results) {
     if (results.length > 0) {
+        nose_x = results[0].pose.nose.x - 15;
+        nose_y = results[0].pose.nose.y - 15;
         console.log(results);
-        console.log("nose x -" + results[0].pose.nose.x);
-        console.log("nose y -" + results[0].pose.nose.y);
+        console.log("nose x -" + nose_x);
+        console.log("nose y -" + nose_y);
     }
 }
 function takeSnapshot() {
@@ -23,4 +28,10 @@ function takeSnapshot() {
 }
 function draw() {
     image(video, 0, 0, 300, 300);
+    image(mustache, nose_x, nose_y, 30, 30);
+
+
+}
+function preload() {
+    mustache = loadImage('https://i.postimg.cc/ryZBhvn9/Mustache0.png');
 }
